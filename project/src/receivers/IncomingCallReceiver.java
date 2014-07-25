0a0067e44c385d0c1d247405ba9cee559b6287e3
Remerico Cruz <remerico@gmail.com>
2014-07-25 18:19:35 +0800

    Auto-reject incoming call

--- project/src/receivers/IncomingCallReceiver.java
+++ project/src/receivers/IncomingCallReceiver.java
@@ -1,9 +1,7 @@
 package receivers;
 
-import events.EventBus;
-import events.IncomingCallEvent;
-import events.MissedCallEvent;
-import android.app.PendingIntent;
+import java.lang.reflect.Method;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +10,13 @@ import android.telephony.TelephonyManager;
 import android.telephony.gsm.SmsManager;
 import android.widget.Toast;
 import app.Preferences;
+
+import com.android.internal.telephony.ITelephony;
+
+import events.EventBus;
+import events.IncomingCallEvent;
+import events.MissedCallEvent;
+
 
 public class IncomingCallReceiver extends BroadcastReceiver {
    
@@ -27,49 +32,76 @@ public class IncomingCallReceiver extends BroadcastReceiver {
      
      
      @Override
-     public void onReceive(Context mContext, Intent intent){
-         
-            // Get the current Phone State
-           String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+     public void onReceive(Context context, Intent intent) {
+
+    	 // Get the current Phone State
+    	 String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+       
+    	 if (state == null) return;
+
+    	 // If phone state "Ringing"
+    	 if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {           
+    		 ring = true;
            
-           if (state == null) return;
-
-           // If phone state "Ringing"
-           if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {           
-        	   ring =true;
-               
-        	   // Get the Caller's Phone Number
-               Bundle bundle = intent.getExtras();
-               callerPhoneNumber= bundle.getString("incoming_number");
-               
-               EventBus.getInstance().post(new IncomingCallEvent(callerPhoneNumber));
-           }
-
-
-            // If incoming call is received
-           if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-        	   callReceived = true;
-           }
+        	 // Get the Caller's Phone Number
+        	 Bundle bundle = intent.getExtras();
+        	 callerPhoneNumber = bundle.getString("incoming_number");
+           
+        	 EventBus.getInstance().post(new IncomingCallEvent(callerPhoneNumber));
+        	 
+        	 endCall(context);
+        	 
+        	 sendSMS(context, callerPhoneNumber);
+        	 
+    	 }
 
 
-            // If phone is Idle
-           if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+    	 // If incoming call is received
+    	 if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+    		 callReceived = true;
+    	 }
 
-        	   // If phone was ringing(ring=true) and not received(callReceived=false) , then it is a missed call
 
-        	   if (ring && !callReceived) {
-        		   
-        		   EventBus.getInstance().post(new MissedCallEvent(callerPhoneNumber));
-        		   
-        		   sendSMS(mContext, callerPhoneNumber);
-        		   
-        		   //Toast.makeText(mContext, "Missed call : "+callerPhoneNumber, Toast.LENGTH_LONG).show();
-        	   }
+    	 // If phone is Idle
+    	 if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+
+    	   // Handle missed call
+    	   if (ring && !callReceived) {
+    		   EventBus.getInstance().post(new MissedCallEvent(callerPhoneNumber));
+    		   //sendSMS(context, callerPhoneNumber);
+    	   }
+    	   
+    	   ring = false;
+    	   callReceived = false;
         	   
          }
            
      }
      
+     
+     
+     @SuppressWarnings("unchecked")
+	private void endCall(Context context) {
+    	 
+    	 TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    	 
+    	 try {
+        	 
+        	 Class c = Class.forName(telephony.getClass().getName());
+             Method m = c.getDeclaredMethod("getITelephony");
+             m.setAccessible(true);
+        	 ITelephony telephonyService = (ITelephony) m.invoke(telephony);
+        	 
+        	 telephonyService = (ITelephony) m.invoke(telephony);
+             telephonyService.endCall();
+        	 
+    	 }
+    	 catch (Exception e) {
+    		 Toast.makeText(context, "Could not end call: " + e.getMessage(), Toast.LENGTH_LONG).show();
+    		 e.printStackTrace();
+    	 }
+    	 
+     }
      
      
      @SuppressWarnings("deprecation")
